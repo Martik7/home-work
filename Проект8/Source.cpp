e2be2a96a4c159a6b3a6dc4bf89c6e7736ba2fad
Олжас <@->
2018-12-25 15:32:59 +0600

    Добавьте файлы проекта.

---
+++ Проект8/Source.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <iostream>
+#include <locale.h>
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	/*int x, y, r;
+	printf("¬ведите х=");
+	scanf_s("%d", &x);
+	printf("¬ведите y=");
+	scanf_s("%d", &y);
+
+	if (x > y)
+	{
+		r = x - y;
+		printf("–езультат = %d\n", r);
+	}
+	else
+	{
+		r = y - x;
+		printf("–езультат = -%d\n", r);
+	}*/
+
+	/*int x, y, r;
+	printf("¬ведите х=");
+	scanf_s("%d", &x);
+	printf("¬ведите y=");
+	scanf_s("%d", &y);
+
+	if (x > y)
+	{
+		r = x / y;
+		printf("–езультат = %d\n",r);
+	}
+	else
+	{
+		r = y / x;
+		printf("–езультат = %d\n", r);
+	}*/
+
+	/*int x, y, r;
+	printf("¬ведите х=");
+	scanf_s("%d", &x);
+	printf("¬ведите y=");
+	scanf_s("%d", &y);
+
+	if(y==0)
+	{
+		r = 12 / x;
+		printf("–езультат = %d", r);
+	}
+	else if (x == 0)
+	{
+		r = 12 / y;
+		printf("–езультат = %d", r);
+	}
+	else if (x != 0 && y != 0)
+	{
+		r = 144 / (x*y);
+		printf("–езультат = %d", r);
+	}*/
+
+	/*int a, d, q, r;
+	printf("¬ведите число а = ");
+	scanf_s("%d", &a);
+	printf("¬ведите число d = ");
+	scanf_s("%d", &d);
+
+	q = a / d;
+	printf("–езультат = %d\n", q);
+
+	r = a%d;
+	printf("ќстаток = %d\n", r);*/
+
+	system("pause");
+}
